095e1d2ddf705e6adcfe64d32a166654ae620315
Mustafa <karimi.mu99@gmail.com>
2023-04-16 16:45:21 +0430

    about page ui done

--- resources/views/landing/about/index.blade.php
+++ resources/views/landing/about/index.blade.php
@@ -24,52 +24,22 @@
         </ol>
       </div>
     </nav>
-  </div><!-- End Breadcrumbs -->
+   </div>
 
 
+{{-- About us section --}}
 @include('landing.about.partials.aboutus')
 
-    <!-- ======= Stats Counter Section ======= -->
-    <section id="stats-counter" class="stats-counter pt-0">
-      <div class="container" data-aos="fade-up">
+<!-- ======= Stats Counter Section ======= -->
+@include('landing.about.partials.statuscounter')
 
-        <div class="row gy-4">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-              <p>Clients</p>
-            </div>
-          </div><!-- End Stats Item -->
-
-          <div class="col-lg-3 col-md-6">
-            <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
-              <p>Projects</p>
-            </div>
-          </div><!-- End Stats Item -->
-
-          <div class="col-lg-3 col-md-6">
-            <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
-              <p>Hours Of Support</p>
-            </div>
-          </div><!-- End Stats Item -->
-
-          <div class="col-lg-3 col-md-6">
-            <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
-              <p>Workers</p>
-            </div>
-          </div><!-- End Stats Item -->
-
-        </div>
-
-      </div>
-    </section><!-- End Stats Counter Section -->
-
+{{-- our team section --}}
 @include('landing.about.partials.team')
+
+{{-- testemonial section --}}
 @include('landing.about.partials.testimonial')
+
+{{-- partial section --}}
 @include('landing.about.partials.faq')
 
 </main>
