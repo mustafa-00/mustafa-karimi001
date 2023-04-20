54fbeace79b6ea2826f1a7d3e592c9505520e265
mustafa-00 <karimi.mu99@gmail.com>
2023-04-20 23:48:02 +0430

    admin and landing services page done

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -9,6 +9,7 @@
         </a>
       </li>
 
+      {{-- HOME --}}
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#home-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Home</span><i class="bi bi-chevron-down ms-auto"></i>
@@ -23,6 +24,7 @@
         </ul>
       </li>
 
+      {{-- ABOUT --}}
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#about-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>About</span><i class="bi bi-chevron-down ms-auto"></i>
@@ -40,11 +42,20 @@
         </ul>
       </li>
 
+      {{-- SERIVCES --}}
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{route('featured.index')}}">
-          <i class="bi bi-grid"></i>
-          <span>Featured</span>
+        <a class="nav-link collapsed" data-bs-target="#services-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-menu-button-wide"></i><span>Services</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
+        <ul id="services-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="{{route('featured.index')}}">
+                  <i class="bi bi-circle"></i>
+                  <span>Featured</span>
+                </a>
+            </li>
+        </ul>
       </li>
+
     </ul>
   </aside>
