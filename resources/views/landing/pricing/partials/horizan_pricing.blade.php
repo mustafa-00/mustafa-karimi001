4cafab785d1c43567d2a228cb9f38c20e69a2303
Mustafa <karimi.mu99@gmail.com>
2023-05-08 18:56:23 +0430

    horizintal done

--- resources/views/landing/pricing/partials/horizan_pricing.blade.php
+++ resources/views/landing/pricing/partials/horizan_pricing.blade.php
@@ -7,45 +7,24 @@
       </div>
 
       <div class="row gy-4 pricing-item" data-aos="fade-up" data-aos-delay="100">
-        @foreach ($hprices as $item)
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <h3>{{$item->tittle}}</h3>
-            </div>
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <h4><sup>$</sup>{{$item->price}}<span> / month</span></h4>
-            </div>
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <ul>
-                <li><i class="bi bi-check"></i>{{$item->markeddescription}}</li>
-                <li><i class="bi bi-check"></i>{{$item->markeddescription}}</li>
-                <li class="na"><i class="bi bi-x"></i> <span>{{$item->unmarkeddescription}}</span></li>
-            </ul>
-            </div>
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <div class="text-center"><a href="#" class="buy-btn">Buy Now</a></div>
-            </div>
-        @endforeach
+            @foreach ($hprices as $item)
+                <div class="col-lg-3 d-flex align-items-center justify-content-center">
+                   <h3>{{$item->tittle}}</h3>
+                </div>
+                <div class="col-lg-3 d-flex align-items-center justify-content-center">
+                   <h4><sup>$</sup>{{$item->price}}<span> / month</span></h4>
+                </div>
+                <div class="col-lg-3 d-flex align-items-center justify-content-center">
+                    <ul>
+                        <li><i class="bi bi-check"></i>{{$item->markeddescription}}</li>
+                        <li><i class="bi bi-check"></i>{{$item->markeddescription}}</li>
+                        <li class="na"><i class="bi bi-x"></i> <span>{{$item->unmarkeddescription}}</span></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 d-flex align-items-center justify-content-center">
+                    <div class="text-center"><a href="#" class="buy-btn">Buy Now</a></div>
+                </div>
+            @endforeach
       </div>
-
-      {{-- {{-- <div class="row gy-4 pricing-item featured mt-2" data-aos="fade-up" data-aos-delay="200">
-        @foreach ($hprices as $item)
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <h3>{{$item->tittle}}</h3>
-            </div>
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <h4><sup>$</sup>{{$item->price}}<span> / month</span></h4>
-            </div>
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <ul>
-                <li><i class="bi bi-check"></i>{{$item->markeddescription}}</li>
-                <li><i class="bi bi-check"></i> <strong>{{$item->markeddescription}}</strong></li>
-                <li><i class="bi bi-check"></i>{{$item->unmarkeddescription}}</li>
-            </ul>
-            </div>
-            <div class="col-lg-3 d-flex align-items-center justify-content-center">
-            <div class="text-center"><a href="#" class="buy-btn">Buy Now</a></div>
-            </div>
-        @endforeach
-        </div> --}}
     </div>
   </section>
